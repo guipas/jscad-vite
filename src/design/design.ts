4930d36145e754894abffedd6730c9523c67af1d
Guillaume Pasquet <gpasquet@industriousoffice.com>
2024-07-27 15:26:05 -0400

    screenshot

--- src/design/design.ts
+++ src/design/design.ts
@@ -2,14 +2,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { subtract } from "@jscad/modeling/src/operations/booleans";
 import { translate } from "@jscad/modeling/src/operations/transforms";
-import { roundedCuboid, sphere } from "@jscad/modeling/src/primitives";
+import { cuboid, sphere } from "@jscad/modeling/src/primitives";
 import { GeometriesTree } from "../types";
 
 export const design = (): GeometriesTree => {
-  const cube = roundedCuboid({ size: [100, 100, 100], roundRadius: 10 });
-  const ball = translate([100 / 2, 0, 0], sphere({ radius: 50 }));
+  const cube = cuboid({ size: [100, 100, 100] });
+  const aSphere = translate([100 / 2, 0, 0], sphere({ radius: 50 }));
 
-  const final = subtract(cube, ball);
+  const final = subtract(cube, aSphere);
 
   // return an object where each property is a JSCAD geometry you want to visualize
   // the property named `final` is your final design
@@ -17,6 +17,6 @@ export const design = (): GeometriesTree => {
   // this is usefull for debugging operations like subtract, as you can see the object that was subtracted
   return {
     final,
-    ball,
+    sphere: aSphere,
   };
 };
